4d5f8a84823269518b6382c06f52b569be91a4d8
nitharsaa <nithuambal07@gmail.com>
2025-08-20 16:39:42 +1000

    Update config_catsL375.js

--- config/config_catsL375.js
+++ config/config_catsL375.js
@@ -45,8 +45,8 @@ var CONFIG = {
   ESTIMATED_TOTAL_DURATION: 15, // minutes
   TOTAL_BLOCKS: 3, // make sure this matches the numbers assigned in the stimulus info below
   BREAK_LENGTH: 10000, // ms length of break between blocks
-  REWARD_AMOUNT: 20, // cents; set to null if no monetary reward
-  TOTAL_REWARD: 15.80, // dollars
+  REWARD_AMOUNT: 5, // cents; set to null if no monetary reward
+  TOTAL_REWARD: 5.85, // dollars
   PLAY_REWARD_AUDIO: true,
   REWARD_IMAGE: 'img/reward.gif', // the image to show on a reward trial
   REWARD_SOUND: 'mp3/reward-coin-drop.mp3', // path to file (use mp3 if possible for browser compatibility), set to null if no sound
